6819a7f1b51096f2875005005ca81e20e953d619
Jisang you <jisangman89@gmail.com>
2017-09-16 23:48:06 +0900

    self-studying

---
+++ src/d/collection/StackSample.java
@@ -0,0 +1,34 @@
+package d.collection;
+
+import java.util.Stack;
+
+public class StackSample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		StackSample sample = new StackSample();
+		sample.checkPeek();
+		sample.checkpop();
+	}
+	
+	public void checkPeek(){
+		Stack<Integer> intStack = new Stack<Integer>();
+		for(int loop = 0; loop <5; loop++){
+			intStack.push(loop);
+			System.out.println(intStack.peek());
+		}
+		System.out.println("size="+intStack.size());
+	}
+	
+	public void checkpop(){
+		Stack<Integer> intStack = new Stack<Integer>();
+		for(int loop = 0; loop <5; loop++){
+			intStack.push(loop);
+			System.out.println(intStack.pop());
+		}
+		System.out.println("size="+intStack.size());
+		
+	}
+
+}
